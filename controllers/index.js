d94f84ab80fc6415dd323c586d5ee4ce53001cda
Benjo <beniamino.tartarini@gmail.com>
2020-07-26 11:51:32 +0900

    first commit

---
+++ controllers/index.js
@@ -0,0 +1,7 @@
+const User = require("../models/User");
+
+module.exports = {
+  getIndex: async (req, res, next) => {
+    console.log("index");
+  },
+};
---
+++ controllers/users.js
@@ -0,0 +1,6 @@
+const User = require("../models/User");
+module.exports = {
+  getUsers: async (req, res, next) => {
+    console.log("users");
+  },
+};

aad38d00d6456e59b6f7d99420dab36061494e4e
Benjo <beniamino.tartarini@gmail.com>
2020-08-02 13:13:13 +0900

    add allproducts test

--- client/src/features/products/AllProducts.test.js
+++ client/src/features/products/AllProducts.test.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { render, cleanup, waitFor } from "@testing-library/react";
+import { render, cleanup, waitFor, fireEvent } from "@testing-library/react";
 import { Provider } from "react-redux";
 import AllProducts from "./AllProducts";
 import store from "../../app/store";
 import { server } from "../../mocks/server";
+import CartIcon from "../cart/CartIcon";
+import { MemoryRouter } from "react-router-dom";
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
@@ -15,8 +17,10 @@ describe("AllProducts tests", () => {
   beforeEach(() => {
     return ({ getByText, getByTestId, getAllByText } = render(
       <Provider store={store}>
+        <CartIcon />
         <AllProducts />
-      </Provider>
+      </Provider>,
+      { wrapper: MemoryRouter }
     ));
   });
   test("AllProducts renders succesfully", () => {
@@ -26,5 +30,14 @@ describe("AllProducts tests", () => {
     await waitFor(() => {
       expect(getAllByText(/testProduct/)).toHaveLength(2);
     });
+  });
+  test("Clicking on Add to Cart updates CartIcon", async () => {
+    expect(getByText(/Items in cart/gi)).toHaveTextContent("Items in Cart: 0");
+    let button;
+    await waitFor(() => {
+      button = getAllByText(/add to cart/gi)[0];
+    });
+    fireEvent.click(button);
+    expect(getByText(/Items in cart/gi)).toHaveTextContent("Items in Cart: 1");
   });
 });
